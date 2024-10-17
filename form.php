586103eda8f798cbb144dc96c72254a3238cb49f
Iqbalaja132 <iqbalmuhamadramdhan0@gmail.com>
2024-10-17 15:16:56 +0700

    tugas

---
+++ form.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Formulir</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <!-- Form ini mengirimkan data ke form_action.php -->
+    <form action="form_action.php" method="post">
+        <label for="id">ID :</label>
+        <input type="number" name="id" placeholder="ID" id="id">
+        <br>
+        <label for="nama">Nama :</label>
+        <input type="text" name="nama" placeholder="Nama" id="nama">
+        <br>
+        <label for="email">Email :</label>
+        <input type="email" name="email" placeholder="Email" id="email">
+        <br>
+        <button type="submit" name="kirim">Kirim</button>
+    </form>
+</body>
+
+</html>
---
+++ form_action.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?php
+// Cek apakah tombol kirim sudah ditekan
+if (isset($_POST['kirim'])) {
+    // Validasi input: cek apakah id, nama, dan email kosong
+    if (empty($_POST['id']) || empty($_POST['nama']) || empty($_POST['email'])) {
+        // Jika ada input yang kosong, redirect kembali ke form.php dengan query string error
+        echo "Inputan tidak boleh kosong";
+    } else {
+        // Ambil data dari form
+        $id = ($_POST['id']);
+        $nama = ($_POST['nama']);
+        $email = ($_POST['email']);
+
+        // Tampilkan hasil inputan
+        // echo "ID: " . $id . "<br>";
+        // echo "Nama: " . $nama . "<br>";
+        // echo "Email: " . $email . "<br>";
+    }
+} else {
+    // Jika form tidak disubmit, redirect kembali ke form.php
+    header('Location: form.php');
+    exit();
+}
+?>
+</body>
+</html>
